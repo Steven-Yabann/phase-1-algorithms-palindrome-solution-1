c6d07c52b1e954211563ec542c4bb47e35ab6064
steven Yabann <stevekiptoo1@gmail.com>
2023-04-03 14:19:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+function reverseString(word){
+  return word.split('').reverse().join('')
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord = reverseString(word)
+  return (word === reversedWord)
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string
+  if reversed string is the same as the input
+    return true
+  else
+    return false
 */
 
 /*
@@ -20,6 +31,28 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
 }
 
 module.exports = isPalindrome;
